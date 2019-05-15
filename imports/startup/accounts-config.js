540ce15e46088444f815e09ecb40f3bd59e9e1da
kadrikaarlop <kadri.kaarlop@gmail.com>
2019-05-15 11:25:11 +0300

    Initial commit

---
+++ imports/startup/accounts-config.js
@@ -0,0 +1,12 @@
+/*import { Accounts } from 'meteor/accounts-base';
+
+Accounts.ui.config({
+    requestPermissions: {
+        task: ['move'],
+    },
+    requestOfflineToken: {
+        google: true
+    },
+    passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
+});
+*/
